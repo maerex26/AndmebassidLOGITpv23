869278fb51de5f5cc834efecaee8ea08b4db4a19
maerex26 <148791977+maerex26@users.noreply.github.com>
2025-04-07 11:04:26 +0300

    tabeli loomine SQL laused

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -1,1 +1,26 @@
+--kommentaar
+--SQL SERVER Management Stuudio
+--Connect TO:
+--Server Name: (LocalDB)\MSSQLLocalDB
+-- Authentification 2 tüüpi:
+--1. Windows Auth - localdb admini õigused
+--2. SQL Server Auth - kontrollida varem tehtud kasutajad
 
+
+CREATE DATABASE SudeichenkoLOGITpv23;
+USE SudeichenkoLOGITpv23;
+
+--tabeli loomine
+--identity(1,1) - ise täidab tabeli 1,2,3
+CREATE TABLE inimene(
+inimeneID int Primary Key identity(1,1),
+nimi varchar(50) unique,
+synniaeg date,
+telefon char(12),
+pikkus decimal(5,2),
+opilaskodu bit
+);
+SELECT * FROM inimene;
+
+--tabeli kustutamine
+DROP table ininmene;
