d0374251c85e83cba5ae61e5b5cd48689e3fae40
maerex26 <148791977+maerex26@users.noreply.github.com>
2025-04-08 13:18:30 +0300

    update table

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -24,3 +24,39 @@ SELECT * FROM inimene;
 
 --tabeli kustutamine
 DROP table ininmene;
+
+------------------------------
+
+--tabeli muutmine uue veergu lisamine
+ALTER TABLE elukoht ADD autoID int;
+--foreign key lisamine
+ALTER TABLE elukoht ADd Constraint fk_autoID
+FOREIGN KEY (autoID) 
+references auto(autoID);
+
+
+SELECT * FROM inimene;
+
+CREATE TABLE autod(
+autoID int PRIMARY KEY identity(1,1),
+autoNr varchar(50) unique,
+mudell varchar(50),
+mark varchar(50) unique,
+v_aasta int
+);
+SELECT * FROM autod;
+
+--tabeli muutmine uue veergu lisamine
+ALTER TABLE inimene ADD autoID int;
+--foreign key lisamine
+ALTER TABLE inimene 
+ADd Constraint fk_autod
+FOREIGN KEY (autodID) 
+references autod(autodID);
+
+INSERT INTO inimene
+(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID, autoID)
+VALUES
+('Bomba Fedya', '2020-11-02', '12345678', 182.1, 1, 1, 1)
+;
+
