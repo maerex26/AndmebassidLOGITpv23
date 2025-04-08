e24a13736e9a560e78653bbd8434d3881cd87e0f
maerex26 <148791977+maerex26@users.noreply.github.com>
2025-04-08 13:37:34 +0300

    xampp

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -1,10 +1,10 @@
 --kommentaar
---SQL SERVER Management Stuudio
+--Xampp control panel
 --Connect TO:
---Server Name: (LocalDB)\MSSQLLocalDB
+--Server Name: localhost - 127.0.0.1
 -- Authentification 2 tüüpi:
---1. Windows Auth - localdb admini õigused
---2. SQL Server Auth - kontrollida varem tehtud kasutajad
+-- kasutaja root
+-- parool: ei ole
 
 
 CREATE DATABASE SudeichenkoLOGITpv23;
