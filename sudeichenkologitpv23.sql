c6375dad872f902a72165b9a953327f2f9126b18
maerex26 <148791977+maerex26@users.noreply.github.com>
2025-04-08 14:41:31 +0300

    Add files via upload

---
+++ sudeichenkologitpv23.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Loomise aeg: Aprill 08, 2025 kell 01:39 PL
+-- Serveri versioon: 10.4.32-MariaDB
+-- PHP versioon: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Andmebaas: `sudeichenkologitpv23`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `autod`
+--
+
+CREATE TABLE `autod` (
+  `autoID` int(11) NOT NULL,
+  `autoNr` varchar(50) DEFAULT NULL,
+  `mudell` varchar(50) DEFAULT NULL,
+  `mark` varchar(50) DEFAULT NULL,
+  `v_aasta` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `elukoht`
+--
+
+CREATE TABLE `elukoht` (
+  `elukohtID` int(11) NOT NULL,
+  `elukoht` varchar(50) DEFAULT NULL,
+  `maakond` varchar(50) DEFAULT NULL,
+  `autoID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `elukoht`
+--
+
+INSERT INTO `elukoht` (`elukohtID`, `elukoht`, `maakond`, `autoID`) VALUES
+(1, 'Tallinn', 'Hrjuma', NULL),
+(2, 'Pärnu', 'Pärnumaa', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `inimene`
+--
+
+CREATE TABLE `inimene` (
+  `inimeneID` int(11) NOT NULL,
+  `nimi` varchar(50) DEFAULT NULL,
+  `synniaeg` date DEFAULT NULL,
+  `telefon` char(12) DEFAULT NULL,
+  `pikkus` decimal(5,2) DEFAULT NULL,
+  `opilaskodu` bit(1) DEFAULT NULL,
+  `elukohtID` int(11) DEFAULT NULL,
+  `autoID` int(11) DEFAULT NULL,
+  `autodID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `inimene`
+--
+
+INSERT INTO `inimene` (`inimeneID`, `nimi`, `synniaeg`, `telefon`, `pikkus`, `opilaskodu`, `elukohtID`, `autoID`, `autodID`) VALUES
+(1, 'Peeter Vana', '2012-12-30', '2568952', 110.50, b'0', 1, NULL, NULL),
+(7, 'Bomba Fedya', '2020-11-02', '12345678', 182.10, b'1', 1, NULL, 1);
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `autod`
+--
+ALTER TABLE `autod`
+  ADD PRIMARY KEY (`autoID`),
+  ADD UNIQUE KEY `autoNr` (`autoNr`),
+  ADD UNIQUE KEY `mark` (`mark`);
+
+--
+-- Indeksid tabelile `elukoht`
+--
+ALTER TABLE `elukoht`
+  ADD PRIMARY KEY (`elukohtID`),
+  ADD UNIQUE KEY `elukoht` (`elukoht`);
+
+--
+-- Indeksid tabelile `inimene`
+--
+ALTER TABLE `inimene`
+  ADD PRIMARY KEY (`inimeneID`),
+  ADD UNIQUE KEY `nimi` (`nimi`),
+  ADD KEY `fk_elukoht` (`elukohtID`),
+  ADD KEY `fk_autod` (`autoID`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `autod`
+--
+ALTER TABLE `autod`
+  MODIFY `autoID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabelile `elukoht`
+--
+ALTER TABLE `elukoht`
+  MODIFY `elukohtID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT tabelile `inimene`
+--
+ALTER TABLE `inimene`
+  MODIFY `inimeneID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Tõmmistatud tabelite piirangud
+--
+
+--
+-- Piirangud tabelile `inimene`
+--
+ALTER TABLE `inimene`
+  ADD CONSTRAINT `fk_autod` FOREIGN KEY (`autoID`) REFERENCES `autod` (`autoID`),
+  ADD CONSTRAINT `fk_elukoht` FOREIGN KEY (`elukohtID`) REFERENCES `elukoht` (`elukohtID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
